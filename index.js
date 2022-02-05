7bb4ebd9e505c890192397c0f057eca94fe88c41
Claudio <claudio.junior233@gmail.com>
2022-02-04 21:03:25 -0300

    fixed routes

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 
-const importData = require("./data.json");
+const importDataCharacters = require("./data.json");
+const importDataTitans = require("./db.json");
 
-const charactersRoutes = require("./characters")
+const charactersRoutes = require("./characters");
+const titanRoutes = require("./titans");
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "*");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET');
-    app.use(cors());
-    next();
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header("Access-Control-Allow-Origin", "*");
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header("Access-Control-Allow-Methods", "GET");
+  app.use(cors());
+  next();
 });
 
-app.get("/" , (req, res) => {
-    res.send('Olá, para fazer a requisição digite /characters. Para fazer requisição de um personagem específico /characters/IDdoPersonagem. By https://github.com/ClaudioDev233 ')
-})
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
-
-app.get('/characters',(req, res) =>{
-     res.status(200).send(importData)
+app.get("/", (req, res) => {
+  res.send(
+    "Olá, para fazer a requisição digite /characters. Para fazer requisição de um personagem específico /characters/IDdoPersonagem. By https://github.com/ClaudioDev233 "
+  );
 });
 
-app.use('/characters', charactersRoutes)
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
 
+app.get("/characters", (req, res) => {
+  res.status(200).send(importDataCharacters);
+});
+app.get("/titans", (req, res) => {
+  res.status(200).send(importDataTitans);
+});
+
+app.use("/characters", charactersRoutes);
+app.use("/titans", titanRoutes);
